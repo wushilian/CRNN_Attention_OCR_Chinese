0b6adc5acd42103e31f2e174692467472f7150cb
wuxiaolian <2501038982@qq.com>
2017-12-26 15:11:33 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,47 @@
+from model import *
+import config as cfg
+import time
+import os
+
+loss,train_decode_result,pred_decode_result=build_network(is_training=True)
+optimizer = tf.train.MomentumOptimizer(learning_rate=cfg.learning_rate, momentum=cfg.momentum, use_nesterov=True)
+train_op=optimizer.minimize(loss)
+saver = tf.train.Saver(max_to_keep=5)
+img,label=cfg.read_data(cfg.train_dir)
+num_train_samples=img.shape[0]
+num_batches_per_epoch = int(num_train_samples/cfg.BATCH_SIZE)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+with tf.name_scope('summaries'):
+    tf.summary.scalar("cost", loss)
+summary_op = tf.summary.merge_all()
+writer = tf.summary.FileWriter(cfg.LOGS_PATH)
+
+target_in,target_out=cfg.label2int(label)
+for cur_epoch in range(cfg.EPOCH):
+    shuffle_idx = np.random.permutation(num_train_samples)
+    train_cost = 0
+    start_time = time.time()
+    batch_time = time.time()
+    # the tracing part
+    for cur_batch in range(num_batches_per_epoch):
+        batch_time = time.time()
+        indexs = [shuffle_idx[i % num_train_samples] for i in
+                  range(cur_batch * cfg.BATCH_SIZE, (cur_batch + 1) * cfg.BATCH_SIZE)]
+        batch_inputs,batch_target_in,batch_target_out=img[indexs],target_in[indexs],target_out[indexs]
+        sess.run( train_op,feed_dict={image: batch_inputs,train_output: batch_target_in,target_output: batch_target_out})
+        if cur_batch%cfg.DISPLAY_STEPS:
+            summary_loss, loss_result = sess.run([summary_op, loss],feed_dict={image: batch_inputs,train_output: batch_target_in,target_output: batch_target_out})
+            writer.add_summary(summary_loss, cur_epoch*num_batches_per_epoch+cur_batch)
+            infer_predict = sess.run(pred_decode_result,feed_dict={image: batch_inputs,train_output: batch_target_in,target_output: batch_target_out})
+            print("epoch:{}, batch:{}, loss:{}, predict_decode:{}, ground_truth:{}".
+                  format(cur_epoch,cur_batch,
+                         loss_result,
+                         cfg.int2label(np.argmax(infer_predict, axis=2)),
+                         cfg.int2label(batch_target_out[0:10])))
+            if not os.path.exists(cfg.CKPT_DIR):
+                os.makedirs(cfg.CKPT_DIR)
+            saver.save(sess, os.path.join(cfg.CKPT_DIR, 'attention_ocr.model'), global_step=cur_epoch*num_batches_per_epoch+cur_batch)
+
+
+
